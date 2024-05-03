4d9cfba308db1a8e3288ff73249657c0675987f4
Thinesh Rasla <76244740+0xrasla@users.noreply.github.com>
2024-05-03 11:05:57 +0530

    Update config.fish

--- linux/config.fish
+++ linux/config.fish
@@ -12,6 +12,10 @@ alias ga="git add"
 
 set -g JAVA_HOME /opt/jdk-19.0.2/
 set -x ANDROID_HOME $HOME/Android/Sdk
+
+set -gx BUN_INSTALL $HOME/.bun
+set -gx PATH $BUN_INSTALL/bin $PATH
+
 set -x PATH $PATH $ANDROID_HOME/tools $ANDROID_HOME/platform-tools $JAVA_HOME/bin
 
 c
